202f040a1c95ed4b8a22198c6e9e0f08e9f11dae
Rabia Kahya <rabiakahya22@gmail.com>
2023-04-30 12:42:08 +0300

    lab

---
+++ react-hooks-react-router-routes-lab-master/src/components/Movies.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { movies } from "../data";
+
+function Movies() {
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie=>{
+        return (
+          <div key={movie.title}>
+            {movie.title}
+            {movie.time}
+            <ul>
+              {movie.genres.map((genre)=> <li key={genre}>{genre}</li>)}
+            </ul>
+          </div>
+        )
+      }))}
+    </div>
+  )
+}
+
+export default Movies;
